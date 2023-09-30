3bd75c8d7541bdbc79ce133cea7cceeed03e541b
DicmaryZambrano <dicmaryzambrano@outlook.com>
2023-09-29 21:18:00 -0400

    Programmed basic xml layout for calculator

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.kotlincalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Declaration of component from xml
+
+        val sum = 66+5
+        val calDisplay: TextView = findViewById(R.id.calDisplay)
+        calDisplay.text = sum.toString()
     }
 }
